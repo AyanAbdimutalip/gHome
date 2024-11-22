c3a2540dc123a1aa940f6033065171e27757ba3a
Ayan Abdimutalip <aianabdimutalip6@gmail.com>
2024-11-22 18:52:44 +0500

    HOWNWORK

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 
-const m = () => {
+// const m = () => {
 
-    let newDistance = 0;
+//     let newDistance = 0;
     
-    while (true) {
+//     while (true) {
 
-        let distance = +prompt("Қашықтықты енгізіңіз... \n Егер '0' деп жазсаңыз сұрау аяқталады")
-        if( distance == 0 ){
-            break;
-        }
+//         let distance = +prompt("Қашықтықты енгізіңіз... \n Егер '0' деп жазсаңыз сұрау аяқталады")
+//         if( distance == 0 ){
+//             break;
+//         }
 
-        let measure = prompt(distance + "қащықтығынын өлшем бірлігін енгізіңіз... \nкилометр - kl\nдециметр - ds\nметр - m")
-            if( measure == "kl") {
-                distance *= 1000;
-                newDistance += distance;
+//         let measure = prompt(distance + "қащықтығынын өлшем бірлігін енгізіңіз... \nкилометр - kl\nдециметр - ds\nметр - m")
+//             if( measure == "kl") {
+//                 distance *= 1000;
+//                 newDistance += distance;
 
-            } else if ( measure == "ds") {
-                distance /= 10;
-                newDistance += distance;
+//             } else if ( measure == "ds") {
+//                 distance /= 10;
+//                 newDistance += distance;
 
-            } else if ( measure == "m") {
-                distance *= 1;
-                newDistance += distance;
+//             } else if ( measure == "m") {
+//                 distance *= 1;
+//                 newDistance += distance;
 
-            }
+//             }
 
-    }
+//     }
 
-    return newDistance
+//     return newDistance
 
-}
+// }
 
-alert("Бүкіл жазған қашықтықтарыңыздың қосындысы: " + m(  ) + " - метр")
+// alert("Бүкіл жазған қашықтықтарыңыздың қосындысы: " + m(  ) + " - метр")
+
+
+
+
